6ae28df1fa788de56dd8c28ec81f4982c6093a65
Rehman Ali <rehmanali001@gmail.com>
2019-08-02 20:31:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class Owner
   # code goes here	
-   attr_accessor :pets:cats, :dogs , 
-  attr_reader :name, :species, 
+   attr_accessor :pets 
+  attr_reader :name, :species, :cats, :dogs
 
    @@all = []
 
